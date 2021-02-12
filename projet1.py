b33fe5c5a4b51a04fb12d2f2ff78826bcac80d85
Evan Sinda <sinda.evan@gmail.com>
2021-02-12 16:59:00 +0100

    Acquisition de l'infomation UPC

---
+++ projet1.py
@@ -0,0 +1,29 @@
+import requests
+import csv342 as csv
+from bs4 import BeautifulSoup
+
+
+Liste =['UPC', 'Titre', 'PrixTaxeInclu', 'PrixTaxeExclu', 'NbrDispon', 'NbrReviews', 'Description', 'Categorie', 'ImageURL']
+
+url = "http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
+response = requests.get(url)
+
+if response.ok:
+    soup = BeautifulSoup(response.text, features="html.parser")
+
+    code = soup.find("td")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
