ec58c0485593f67fb62562c7f13330832ff58e42
Evan Sinda <sinda.evan@gmail.com>
2021-02-23 14:51:52 +0100

    écrire les données dans un fichier CSV distinct pour chaque catégorie de livres

--- projet1.py
+++ projet1.py
@@ -2,82 +2,117 @@ import requests
 import csv342 as csv
 from bs4 import BeautifulSoup
 
+liste_url_categorie = []
+liste_url_livres = []
 
-liste = []
+# Récupération des liens des catégorie dans un fichier txt
 
-
-url = 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html'
+url = 'http://books.toscrape.com/index.html'
 
 response = requests.get(url)
 
 if response.ok:
     soup = BeautifulSoup(response.text, features="html.parser")
-    href = soup.findAll("div", {"class": "image_container"})
-    for div in href:
-        a = div.find('a')
-        lien = a['href']
-        liste.append("http://books.toscrape.com/catalogue" + lien[8:])
+    href = soup.findAll("ul", {"class": "nav nav-list"})
 
-if soup.findAll("li", {"class": "next"}):
-    nxt = soup.findAll("li", {"class": "next"})
-    for li in nxt:
-        b = li.find('a')
-        lien2 = b['href']
+    for li in href:
+        liste_a = li.findAll('a')
+        for a in liste_a:
+            lien = a['href']
+            liste_url_categorie.append("http://books.toscrape.com/" + lien)
+    del liste_url_categorie[0]
 
-    url = url[0:-10] + lien2
+    with open('urls_categorie.txt', 'w') as file:                                                                                   # Ouverture d'un fichier txt
+        for lien in liste_url_categorie:
+            file.write(lien + '\n')
 
+# Accéder à l'url d'une catégorie
 
-    for i in range(2, 8):
-        nv_url = url[0:-6] + str(i) + '.html'
-
-        response2 = requests.get(nv_url)
-
-        if response2.ok:
-            soup = BeautifulSoup(response2.text, features="html.parser")
-            href = soup.findAll("div", {"class": "image_container"})
-            for div in href:
-                a = div.find('a')
-                lien = a['href']
-                liste.append("http://books.toscrape.com/catalogue" + lien[8:])
-
-with open('urls.txt', 'w') as file:
-    for link in liste:
-        file.write(link + '\n')
-
-
-Liste = ['UPC', 'Titre', 'PrixTaxeInclu', 'PrixTaxeExclu', 'NbrDispon', 'NbrReviews', 'Categorie',
-         'ImageURL', 'URL', 'Description']
-
-with open('test.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(Liste)
-
-with open('urls.txt', 'r') as file:
-    for row in file:
+with open('urls_categorie.txt', 'r') as file:                                                                                        # Lecture du fichier txt
+    for row in file:                                                                                                                 # Pour chaque row dans le fichier
         url = row.strip()
 
-        response = requests.get(url)
+# Récupération des liens de la première page d'une catégorie
+
+        response = requests.get(url)                                                                                                # Requête vers le nouvel url
         if response.ok:
             soup = BeautifulSoup(response.text, features="html.parser")
+            href = soup.findAll("div", {
+                "class": "image_container"})                                                                                        # Chercher toute les div class: image_container dans la page HTML
+            for div in href:                                                                                                        # Chercher les elements div dans la page
+                a = div.find('a')                                                                                                   # Pour chaque élément div on va chercher le a
+                lien = a['href']                                                                                                    # Pour chaque a on va chercher le href
+                liste_url_livres.append("http://books.toscrape.com/catalogue/" + lien[9:])                                                        # Ajout du lien reconstitué à la liste
 
-            code = soup.find("td")
 
-            title = soup.find("h1")
+# Récupération des liens des autres pages
 
-            price_exclu_tax = soup.findAll("td")[2]
+            if soup.find("li", {"class": "next"}):                                                                                      # Si on trouve dans l'objet soup, la balise (li) avec la class "next"
+                nxt = soup.findAll("li", {"class": "next"})                                                                             # nxt devient la balise (li) avec la class "next"
+                for li in nxt:                                                                                                          # Chercher les elements li dans la page
+                    b = li.find('a')                                                                                                    # Pour chaque élément li on va chercher le a
+                    lien2 = b['href']
 
-            price_incl_tax = soup.findAll("td")[3]
+                    url = url[0:-10] + lien2
 
-            number_available = soup.findAll("td")[5]
+                    for i in range(2, 8):                                                                                               # Pour I allant de 2 a 8 (car 8 pages maximum)
+                        nv_url = url[0:-6] + str(i) + '.html'                                                                           # Nouvel URL = ancien en remplacant le numéro de page par i
 
-            number_reviews = soup.findAll("td")[6]
 
-            category = soup.findAll("li")[2]
+                        response = requests.get(nv_url)                                                                                     # Requête vers l'URL
 
-            image_url = soup.find("img")["src"]
+                        if response.ok:                                                                                                     # Même bloc de code que précédement
+                            soup = BeautifulSoup(response.text, features="html.parser")                                                                    # Creation de l'objet soup avec le code HTML dedans
+                            href = soup.findAll("div", {"class": "image_container"})                                                                                # Chercher toute les div class: image_container dans la page HTML
+                            for div in href:                                                                                                # Chercher les elements div dans la page
+                                a = div.find('a')                                                                                           # Pour chaque élément div on va chercher le a
+                                lien = a['href']                                                                                            # Pour chaque a on va chercher le href
+                                liste_url_livres.append("http://books.toscrape.com/catalogue/" + lien[9:])
 
-            description = soup.findAll("p")[3]
+# Ecriture dans le fichiers txt de la liste contenant les liens
 
-            with open('test.csv', 'a', newline='', encoding='utf-8') as csvfile:
+with open('urls_livres.txt', 'w') as file2:                                                                         # Ouverture d'un fichier txt
+    for lien in liste_url_livres:
+        file2.write(lien + '\n')                                                                             # Ecrite dans le fichier texte les liens
+
+
+# Création et écriture dans le fichier csv des informations souhaités pour chaque URL enregistré dans le fichier txt
+
+Liste = ['UPC', 'Titre', 'PrixTaxeInclu', 'PrixTaxeExclu', 'NbrDispon', 'NbrReviews', 'Categorie',
+         'ImageURL', 'URL', 'Description']                                                                  # Création d'un liste
+
+with open('test.csv', 'w', newline='', encoding='utf-8') as csvfile:                                        # Creation du fichier csv
+    spamwriter = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)               # !!!!!!!!!
+    spamwriter.writerow(Liste)                                                                              # Ecriture de la Liste dans le fichier CSV
+
+with open('urls_livres.txt', 'r') as file2:                                                                         # Lecture du fichier txt
+    for row in file2:                                                                                        # Pour chaque row dans le fichier
+        url = row.strip()                                                                                   # URL prends la valeur de row
+
+        response = requests.get(url)                                                                        # Requête vers le nouvel url
+        if response.ok:
+            soup = BeautifulSoup(response.text, features="html.parser")                                     # Creation de l'objet soup avec le code html dedans
+
+            code = soup.find("td")                                                                          # Chercher le premier td
+
+            title = soup.find("h1")                                                                         # Chercher h
+
+            price_exclu_tax = soup.findAll("td")[2]                                                         # Chercher le deuxième td
+
+            price_incl_tax = soup.findAll("td")[3]                                                          # Chercher le troisième td
+
+            number_available = soup.findAll("td")[5]                                                        # Chercher le cinquième td
+
+            number_reviews = soup.findAll("td")[6]                                                          # Chercher le sixième td
+
+            category = soup.findAll("li")[2]                                                                # Chercher le deuxième li
+
+            image_url = soup.find("img")["src"]                                                             # Chercher l'URL de l'image
+
+            description = soup.findAll("p")[3]                                                              # Chercher le troisième p
+
+            file_name = category.text[1:-1] + '.csv'
+
+            with open(file_name, 'a', newline='', encoding='utf-8') as csvfile:                            # Ecriture dans le fichier csv des données récupérés
                 spamwriter = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
                 spamwriter.writerow([code.text, title.text, price_exclu_tax.text[1:], price_incl_tax.text[1:], number_available.text, number_reviews.text, category.text[1:-1], "http://books.toscrape.com" + image_url[5:], url, description.text])
